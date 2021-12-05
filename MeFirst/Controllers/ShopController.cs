c6ab551a62e6be0fe78ef744147cf4968fe01f2a
jenni <jenni@DESKTOP-RE2HD7E>
2021-12-04 19:27:02 -0500

    Adding a shop feature where annon users can use. Only problem is the usablitiy of it. Need to figure our how to link dry, combo, aging skin

---
+++ MeFirst/Controllers/ShopController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeFirst.Data;
+
+
+namespace MeFirst.Controllers
+{
+    public class ShopController : Controller
+    {
+        // adding a db connection
+        private readonly ApplicationDbContext _context;
+        // constructor - every instance of this class requires a DbContext object
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context; // making db connetction public in this class. SO we can use in any method. 
+        }
+        public IActionResult Index()
+        {
+            // passing a list of skintypes where users can shop from their skin type. 
+            var SkinTypes = _context.SkinTypes.OrderBy(c => c.TreatmentsID).ToList();
+            return View(SkinTypes);
+        }
+    }
+}
